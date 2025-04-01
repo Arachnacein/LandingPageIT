547ea8503473c7b559d4c03b95faa325c63da807
Jakub <jakub26800@gmail.com>
2025-04-01 18:57:52 +0200

    Added studies component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { VideoComponent } from './components/video/video.component';
 import { TechstackComponent } from './components/techstack/techstack.component';
+import { StudiesComponent } from './components/studies/studies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, VideoComponent, TechstackComponent],
+  imports: [NavbarComponent, HomeComponent, VideoComponent, TechstackComponent, StudiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
