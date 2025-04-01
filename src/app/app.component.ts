f3a53321b87be5243e6461498230801474918567
Jakub <jakub26800@gmail.com>
2025-04-01 21:27:10 +0200

    Added work component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { VideoComponent } from './components/video/video.component';
 import { TechstackComponent } from './components/techstack/techstack.component';
 import { StudiesComponent } from './components/studies/studies.component';
+import { WorkComponent } from './components/work/work.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, VideoComponent, TechstackComponent, StudiesComponent],
+  imports: [NavbarComponent, HomeComponent, VideoComponent, TechstackComponent, StudiesComponent, WorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'LandingPageIT';
+  title = 'Jakub Łuka';
 }
